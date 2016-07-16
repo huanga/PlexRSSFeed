c2adf31fd7412a68cf6f94547e3e7c769443995e
Andy Huang <ahuang@bbtv-8nrce-2.local>
2016-07-15 19:31:14 -0700

    Force a flush even if maxBufferLevel is null; we have an extra buffer some where.

--- src/RSSEmitter.php
+++ src/RSSEmitter.php
@@ -3,17 +3,20 @@ namespace PlexRSSFeed;
 
 use Psr\Http\Message\ResponseInterface;
 use Zend\Diactoros\Response\EmitterInterface;
+use Zend\Diactoros\Response\SapiEmitterTrait;
 
 class RSSEmitter implements EmitterInterface
 {
+    use SapiEmitterTrait;
+
     public function emit(ResponseInterface $response, $maxBufferLevel = null)
     {
         $response = $this->injectContentLength($response);
 
         $this->emitStatusLine($response);
         $this->emitHeaders($response);
-        $this->flush($maxBufferLevel);
         $this->emitBody($response);
+        $this->flush(0);
     }
 
     private function emitBody(ResponseInterface $response)
